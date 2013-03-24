91a49231c942a8cfe8b814d5982663d700346862
Anthony Johnston <aj@antix.co.uk>
2013-03-24 19:50:11 +0000

    string.ToSafeFileName implementation added in Antix.IO

---
+++ source/Antix.Tests/ToSafeFileNameTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Antix.IO;
+using Xunit;
+
+namespace Antix.Tests
+{
+    public class ToSafeFileNameTests
+    {
+        [Fact]
+        public void ReplacesInvalidWithDefault()
+        {
+            var result = "thing\\whatsit".ToSafeFileName();
+
+            Assert.Equal("thing_whatsit", result);
+        }
+
+        [Fact]
+        public void ReplacesInvalidWithSpecifiedReplacement()
+        {
+            var result = "thing\\whatsit".ToSafeFileName('-');
+
+            Assert.Equal("thing-whatsit", result);
+        }
+
+        [Fact]
+        public void SpecifiedReplacementMustBeValid()
+        {
+            Assert.Throws<ArgumentException>(
+                () => "thing\\whatsit".ToSafeFileName('\\')
+                );
+        }
+
+        [Fact]
+        public void ValueCannotBeNull()
+        {
+            Assert.Throws<ArgumentException>(
+                () => default(string).ToSafeFileName()
+                );
+        }
+
+        [Fact]
+        public void ValueCannotBeEmpty()
+        {
+            Assert.Throws<ArgumentException>(
+                () => string.Empty.ToSafeFileName()
+                );
+        }
+
+        [Fact]
+        public void ValueCannotBeWhitespace()
+        {
+            Assert.Throws<ArgumentException>(
+                () => "  ".ToSafeFileName()
+                );
+        }
+    }
+}
