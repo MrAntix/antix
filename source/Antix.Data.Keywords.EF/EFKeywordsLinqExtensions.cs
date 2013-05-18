f7861112bf412ae94560df9deef9974cf20fcfee
Anthony Johnston <aj@antix.co.uk>
2013-05-18 19:41:33 +0100

    Removed overload using static processor, could lead to confusion over which is being used

--- source/Antix.Data.Keywords.EF/EFKeywordsLinqExtensions.cs
+++ source/Antix.Data.Keywords.EF/EFKeywordsLinqExtensions.cs
@@ -7,9 +7,6 @@ namespace Antix.Data.Keywords.EF
 {
     public static class EFKeywordsLinqExtensions
     {
-        public static IKeywordProcessor KeywordProcessor
-            = WordSplitKeywordProcessor.Create();
-
         /// <summary>
         ///     <para>
         ///         Find items of type <see cref="T" /> which have matching keywords for the text passed
@@ -26,7 +23,7 @@ namespace Antix.Data.Keywords.EF
         /// <param name="text">Text to find</param>
         /// <param name="keywordProcessor">Processes the text into keywords to search on</param>
         /// <returns>Matching ranked elements</returns>
-        public static IQueryable<T> Matches<T>(
+        public static IQueryable<T> Match<T>(
             this IQueryable<T> source,
             string text,
             IKeywordProcessor keywordProcessor)
@@ -40,41 +37,15 @@ namespace Antix.Data.Keywords.EF
 
             return
                 from e in source
-                orderby
+                let rank =
                     (
                         from ek in e.Keywords
                         where keywords.Any(k => k == ek.Keyword.Value)
-                        select ek.Frequency/(decimal) ek.Keyword.Frequency
-                    ).Sum() descending
+                        select ek.Frequency / (decimal)ek.Keyword.Frequency
+                    ).Sum()
+                where rank > 0
+                orderby rank descending
                 select e;
-        }
-
-        /// <summary>
-        ///     <para>
-        ///         Find items of type <see cref="T" /> which have matching keywords for the text passed
-        ///     </para>
-        ///     <para>
-        ///         Text is processed by the static <see cref="KeywordProcessor" />, which should be the same one that builds the keywords for the elements
-        ///     </para>
-        ///     <para>
-        ///         The search is ranked by Tf-idf <seealso cref="http://en.wikipedia.org/wiki/Tf%E2%80%93idf" />
-        ///     </para>
-        /// </summary>
-        /// <typeparam name="T">Entity Type</typeparam>
-        /// <param name="source">
-        ///     Queryable source of <see cref="T" />
-        /// </param>
-        /// <param name="text">Text to find</param>
-        /// <returns>Matching ranked elements</returns>
-        public static IQueryable<T> Matches<T>(
-            this IQueryable<T> source,
-            string text)
-            where T : IndexedEntity
-        {
-            Contract.Requires(source != null);
-            Contract.Requires(!string.IsNullOrWhiteSpace(text));
-
-            return Matches(source, text, KeywordProcessor);
         }
     }
 }
