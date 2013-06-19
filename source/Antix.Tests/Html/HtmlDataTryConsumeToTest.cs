8176a41410cc0ab26cf534f7d085417f7b0e4fab
Anthony Johnston <aj@antix.co.uk>
2013-06-19 17:44:27 +0100

    GetReader allow me to swap out concrete

--- source/Antix.Tests/Html/HtmlDataTryConsumeToTest.cs
+++ source/Antix.Tests/Html/HtmlDataTryConsumeToTest.cs
@@ -6,57 +6,62 @@ namespace Antix.Tests.Html
 {
     public class HtmlDataTryConsumeToTest
     {
+        static IHtmlReader GetReader(string htmlString)
+        {
+            return new HtmlReader(htmlString);
+        }
+
         [Fact]
         public void consume_some()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.Consume(2);
+            var result = reader.Consume(2);
 
             Assert.Equal("ab", result);
-            Assert.Equal('c', queue.Peek());
+            Assert.Equal('c', reader.Peek());
         }
 
         [Fact]
         public void consume_too_many()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
             Assert.Throws<InvalidOperationException>(
-                () => queue.Consume(5));
+                () => reader.Consume(5));
         }
 
         [Fact]
         public void try_consume_succeeds_and_consumes()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("b", true, true);
+            var result = reader.TryConsume("b", true, true);
 
             Assert.True(result);
 
-            Assert.Equal('c', queue.Peek());
+            Assert.Equal('c', reader.Peek());
         }
 
         [Fact]
         public void try_consume_fails_and_does_not_consume()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("x", true, true);
+            var result = reader.TryConsume("x", true, true);
 
             Assert.False(result);
 
-            Assert.Equal('a', queue.Peek());
+            Assert.Equal('a', reader.Peek());
         }
 
         [Fact]
         public void consumed_does_not_include_the_target()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
             string consumed;
-            queue.TryConsume("c", true, true, out consumed);
+            reader.TryConsume("c", true, true, out consumed);
 
             Assert.Equal("ab", consumed);
         }
@@ -64,73 +69,73 @@ namespace Antix.Tests.Html
         [Fact]
         public void nothing_to_find()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("", true, true);
+            var result = reader.TryConsume("", true, true);
 
             Assert.True(result);
 
-            Assert.Equal('a', queue.Peek());
+            Assert.Equal('a', reader.Peek());
         }
 
         [Fact]
-        public void not_in_queue()
+        public void not_in_reader()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("x", true, true);
+            var result = reader.TryConsume("x", true, true);
 
             Assert.False(result);
 
-            Assert.Equal('a', queue.Peek());
+            Assert.Equal('a', reader.Peek());
         }
 
         [Fact]
         public void string_at_beginning()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("ab", true, true);
+            var result = reader.TryConsume("ab", true, true);
 
             Assert.True(result);
 
-            Assert.Equal('c', queue.Peek());
+            Assert.Equal('c', reader.Peek());
         }
 
         [Fact]
         public void string_at_end()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("cd", true, true);
+            var result = reader.TryConsume("cd", true, true);
 
             Assert.True(result);
 
-            Assert.False(queue.Any());
+            Assert.False(reader.Any());
         }
 
         [Fact]
         public void string_at_middle()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
-            var result = queue.TryConsume("bc", true, true);
+            var result = reader.TryConsume("bc", true, true);
 
             Assert.True(result);
 
-            Assert.Equal('d', queue.Peek());
+            Assert.Equal('d', reader.Peek());
         }
 
         [Fact]
         public void string_at_middle_get_consumed()
         {
-            var queue = new HtmlReader("abcd");
+            var reader = GetReader("abcd");
 
             string consumed;
-            queue.TryConsume("bc", true, true, out consumed);
+            reader.TryConsume("bc", true, true, out consumed);
 
             Assert.Equal("a", consumed);
-            Assert.Equal('d', queue.Peek());
+            Assert.Equal('d', reader.Peek());
         }
     }
 }
