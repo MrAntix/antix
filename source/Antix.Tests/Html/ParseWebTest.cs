d1440acd984b8307e3fc1ac6c34a5795da315190
Anthony Johnston <aj@antix.co.uk>
2013-06-19 20:29:04 +0100

    small page large page test

--- source/Antix.Tests/Html/ParseWebTest.cs
+++ source/Antix.Tests/Html/ParseWebTest.cs
@@ -29,9 +29,27 @@ namespace Antix.Tests.Html
         }
 
         [Fact]
-        public void parse_google_with_string_based_reader()
+        public void parse_small_page_with_string_based_reader()
         {
-            var htmlString = FindResource("Resources.Google.htm");
+            var htmlString = FindResource("Resources.SmallPage.htm");
+
+            IHtmlNode[] html = null;
+            var result = Benchmark.Run(
+                () => html = new HtmlParser(s => new StringHtmlReader(s))
+                                 .Parse(htmlString)
+                                 .ToArray(),
+                5);
+
+            Assert.NotNull(html);
+            Assert.Equal(2, html.Count());
+
+            Console.Write(result);
+        }
+
+        [Fact]
+        public void parse_big_page_with_string_based_reader()
+        {
+            var htmlString = FindResource("Resources.BigPage.htm");
 
             IHtmlNode[] html = null;
             var result = Benchmark.Run(
