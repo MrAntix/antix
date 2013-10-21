a54c159b4eb20ba64b63cb281e9dcb41f49d86a3
Anthony Johnston <aj@antix.co.uk>
2013-10-21 22:31:18 +0100

    StringSearch
    for finding strings in strings or over a number of strings

---
+++ source/Antix.Tests/StringSearchTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+namespace Antix.Tests
+{
+    public class StringSearchTests
+    {
+        [Fact]
+        public void CanFindAtBegining()
+        {
+            const string toSearch = "ABCD";
+
+            var search = new StringSearch("AB");
+
+            search.Execute(toSearch);
+
+            Assert.True(search.IsFound);
+            Assert.Equal(0, search.Start);
+        }
+
+        [Fact]
+        public void CanFindInMiddle()
+        {
+            const string toSearch = "ABCD";
+
+            var search = new StringSearch("BC");
+
+            search.Execute(toSearch);
+
+            Assert.True(search.IsFound);
+            Assert.Equal(1, search.Start);
+        }
+
+        [Fact]
+        public void CanFindAtEnd()
+        {
+            const string toSearch = "ABCD";
+
+            var search = new StringSearch("CD");
+
+            search.Execute(toSearch);
+
+            Assert.True(search.IsFound);
+            Assert.Equal(2, search.Start);
+        }
+
+
+        [Fact]
+        public void CanFindBetween()
+        {
+            const string toSearch1 = "ABCD";
+            const string toSearch2 = "EFGH";
+
+            var search = new StringSearch("CDEFGH");
+
+            search.Execute(toSearch1);
+
+            Assert.False(search.IsFound);
+            Assert.Equal(2, search.Start);
+            Assert.Equal(2, search.Index);
+
+            search.Execute(toSearch2);
+
+            Assert.True(search.IsFound);
+            Assert.Equal(-2, search.Start);
+        }
+    }
+}
