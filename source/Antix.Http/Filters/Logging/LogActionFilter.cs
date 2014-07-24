7cb0bcb1112f266ed162ca350ddc73b6c2a7ec45
Anthony Johnston <aj@antix.co.uk>
2014-07-24 19:36:19 +0100

    log data in and out

--- source/Antix.Http/Filters/Logging/LogActionFilter.cs
+++ source/Antix.Http/Filters/Logging/LogActionFilter.cs
@@ -8,6 +8,8 @@ using System.Web.Http.Filters;
 using Antix.Logging;
 using Antix.Services;
 
+using Newtonsoft.Json;
+
 namespace Antix.Http.Filters.Logging
 {
     public class LogActionFilter :
@@ -26,14 +28,20 @@ namespace Antix.Http.Filters.Logging
             CancellationToken cancellationToken,
             Func<Task<HttpResponseMessage>> continuation)
         {
-            _log.Debug(m => m("Action {0}.{1}",
+            _log.Debug(m => m("Action {0}.{1} ({2})",
                 actionContext.ActionDescriptor.ControllerDescriptor.ControllerName,
-                actionContext.ActionDescriptor.ActionName
+                actionContext.ActionDescriptor.ActionName,
+                JsonConvert.SerializeObject(actionContext.ActionArguments)
                 ));
 
             try
             {
-                return await continuation();
+                var result = await continuation();
+                _log.Debug(m => m("{0} => {1}",
+                    result.StatusCode,
+                    result.Content.ReadAsStringAsync().Result));
+
+                return result;
             }
             catch (Exception ex)
             {
