d3e4be8713053580714ed12e74df7a49a364ffa4
Anthony Johnston <aj@antix.co.uk>
2014-10-04 20:49:22 +0100

    added errors collection to the response object

---
+++ source/Antix.Services/Models/Response.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Antix.Services.Models
+{
+    public class Response
+    {
+        readonly IReadOnlyCollection<ResponseError> _errors;
+
+        protected Response(
+            IEnumerable<ResponseError> errors = null)
+        {
+            _errors = errors == null ? new ResponseError[] {} : errors.ToArray();
+        }
+
+        public IReadOnlyCollection<ResponseError> Errors
+        {
+            get { return _errors; }
+        }
+
+        public static Response OK()
+        {
+            return new Response();
+        }
+
+        public static Response<T> OK<T>(T data)
+        {
+            return new Response<T>(data);
+        }
+
+        public static Response WithErrors(IEnumerable<ResponseError> errors)
+        {
+            return new Response(errors);
+        }
+
+        public static Response<T> WithErrors<T>(T data, IEnumerable<ResponseError> errors)
+        {
+            return new Response<T>(data, errors);
+        }
+    }
+
+    public class Response<T> : Response
+    {
+        readonly T _data;
+
+        internal Response(
+            T data,
+            IEnumerable<ResponseError> errors = null) :
+                base(errors)
+        {
+            _data = data;
+        }
+
+        public T Data
+        {
+            get { return _data; }
+        }
+    }
+}
---
+++ source/Antix.Services/Models/ResponseError.cs
@@ -0,0 +1,8 @@
+﻿namespace Antix.Services.Models
+{
+    public class ResponseError
+    {
+        public string Code { get; set; }
+        public string Path { get; set; }
+    }
+}
