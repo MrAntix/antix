8c2e6111f5bca1c01ade599cb98f0366050e9b79
Anthony Johnston <aj@antix.co.uk>
2014-10-04 23:14:46 +0100

    Response Errors Summary extension

--- source/Antix.Services/Models/ResponseError.cs
+++ source/Antix.Services/Models/ResponseError.cs
@@ -4,5 +4,10 @@
     {
         public string Code { get; set; }
         public string Path { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Path, Code);
+        }
     }
 }
---
+++ source/Antix.Services/Models/ResponseExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Antix.Services.Models
+{
+    public static class ResponseExtensions
+    {
+        public static string ToSummary(this IEnumerable<ResponseError> errors)
+        {
+            return string.Join("\n", errors.Select(e => e.ToString()));
+        }
+    }
+}
