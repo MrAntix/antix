ca4615323fc52457c80f0c89e9658ed41c058749
Anthony Johnston <aj@antix.co.uk>
2015-01-31 14:16:34 +0000

    corrected range logic

---
+++ source/Antix.Tests/Services/Validation/Predicates/RangePredicateTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Antix.Services.Validation.Predicates;
+using Xunit;
+
+namespace Antix.Tests.Services.Validation.Predicates
+{
+    public class RangePredicateTests
+    {
+        [Fact]
+        public void date_in_range()
+        {
+            var service = new DateTimeOffsetRangePredicate(
+                DateTimeOffset.UtcNow.AddYears(-100), DateTimeOffset.UtcNow);
+
+            Assert.True(service.Is(DateTimeOffset.UtcNow.AddYears(-1)));
+        }
+    }
+}
