6afd9b6354cfa4cbb437d5767d264b7cf6f839c6
Anthony Johnston <aj@antix.co.uk>
2015-04-18 13:48:44 +0100

    added network code project

---
+++ source/Antix.Net/NetExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Antix.Net
+{
+    public static class NetExtensions
+    {
+        public static IEnumerable<IPAddress> GetLocalIPAddresses(
+            this IEnumerable<NetworkInterface> networkInterfaces, params NetworkInterfaceType[] types)
+        {
+            return from networkInterface in networkInterfaces.WhereUp(types)
+                from unicastIPAddress in networkInterface.GetIPProperties().UnicastAddresses
+                where unicastIPAddress.Address.AddressFamily == AddressFamily.InterNetwork
+                select unicastIPAddress.Address;
+        }
+
+        public static IEnumerable<NetworkInterface> WhereUp(
+            this IEnumerable<NetworkInterface> networkInterfaces, params NetworkInterfaceType[] types)
+        {
+            return networkInterfaces
+                .Where(networkInterface =>
+                    networkInterface.OperationalStatus == OperationalStatus.Up
+                    && (!types.Any() || types.Contains(networkInterface.NetworkInterfaceType))
+                );
+        }
+    }
+}
---
+++ source/Antix.Tests/Net/NetExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using Antix.Net;
+using Xunit;
+
+namespace Antix.Tests.Net
+{
+    public class NetExtensionsTests
+    {
+        [Fact]
+        public void get_local_ip()
+        {
+            var ips = NetworkInterface
+                .GetAllNetworkInterfaces()
+                .GetLocalIPAddresses(NetworkInterfaceType.Ethernet, NetworkInterfaceType.Wireless80211);
+
+            foreach (var ip in ips)
+            {
+                Console.WriteLine(ip);
+            }
+        }
+    }
+}
